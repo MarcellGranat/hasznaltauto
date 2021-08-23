10fe63fe740f7c768c9dfbeaa4eb4132c37aa570
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-08-23 10:41:47 +0200

    [daily update]

--- scrape.bat
+++ scrape.bat
@@ -1,8 +1,6 @@
 cd \
-"Program Files\R\R-4.1.0\bin\R.exe" -e "print("hello world")"
+"Program Files\R\R-4.1.0\bin\R.exe" -e "source('C:/rprojects/hasznaltauto/R/scrape.R', echo=TRUE)"
 cd rprojects\hasznaltauto
 git add .
 git commit -m "[daily update]"
 git push
-pause
-
