99a2c98667e4d7eb0a86879433155f091133395c
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-08-24 15:29:46 +0200

    [daily update]

--- scrape.bat
+++ scrape.bat
@@ -1,6 +1,11 @@
 cd \
+cd rprojects\hasznaltauto
+git pull
+cd \
 "Program Files\R\R-4.1.0\bin\R.exe" -e "source('C:/rprojects/hasznaltauto/R/scrape.R', echo=TRUE)"
+"Program Files\R\R-4.0.3\bin\R.exe" -e "source('C:/rprojects/hasznaltauto/R/scrape.R', echo=TRUE)"
 "Program Files\R\R-4.1.0\bin\R.exe" -e "rmarkdown::render('c:/rprojects/hasznaltauto/README.rmd')"
+"Program Files\R\R-4.0.3\bin\R.exe" -e "rmarkdown::render('c:/rprojects/hasznaltauto/README.rmd')"
 cd rprojects\hasznaltauto
 git add .
 git commit -m "[daily update]"
