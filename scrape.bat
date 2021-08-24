28e9947527924089685f71c03a1d9a3b92543812
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-08-24 19:13:01 +0200

    debug scrape.bat

--- scrape.bat
+++ scrape.bat
@@ -3,8 +3,10 @@ cd rprojects\hasznaltauto
 git pull
 cd \
 "Program Files\R\R-4.1.0\bin\R.exe" -e "source('C:/rprojects/hasznaltauto/R/scrape.R', echo=TRUE)"
+"Program Files\R\R-4.1.1\bin\R.exe" -e "source('C:/rprojects/hasznaltauto/R/scrape.R', echo=TRUE)"
 "Program Files\R\R-4.0.3\bin\R.exe" -e "source('C:/rprojects/hasznaltauto/R/scrape.R', echo=TRUE)"
 "Program Files\R\R-4.1.0\bin\R.exe" -e "rmarkdown::render('c:/rprojects/hasznaltauto/README.rmd')"
+"Program Files\R\R-4.1.1\bin\R.exe" -e "rmarkdown::render('c:/rprojects/hasznaltauto/README.rmd')"
 "Program Files\R\R-4.0.3\bin\R.exe" -e "rmarkdown::render('c:/rprojects/hasznaltauto/README.rmd')"
 cd rprojects\hasznaltauto
 git add .
